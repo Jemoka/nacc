6de897455ff63bf8ed4d43cdf91bf46e5337342e
Houjun Liu <houjun@jemoka.com>
2024-12-16 11:07:05 -0800

    even more often!

--- experiments/e1/e1_1024.sh
+++ experiments/e1/e1_1024.sh
@@ -8,7 +8,7 @@ HIDDEN=1024 # we will sweep through various hidden dimensions
 
 for lr in 5e-3 5e-4 5e-5 5e-6; do
     for featureset in neuralpsych combined; do
-        CUDA_VISIBLE_DEVICES=2 uv run python main.py "e1_${featureset}_${HIDDEN}_${lr}" --hidden_dim $HIDDEN --n_layers $NLAYERS --batch_size $BATCH_SIZE --lr $lr --featureset $featureset --validation_interval 128 --checkpoint_interval 128 --wandb 
+        CUDA_VISIBLE_DEVICES=2 uv run python main.py "e1_${featureset}_${HIDDEN}_${lr}" --hidden_dim $HIDDEN --n_layers $NLAYERS --batch_size $BATCH_SIZE --lr $lr --featureset $featureset --validation_interval 64 --checkpoint_interval 64 --report_interval 16 --wandb 
     done
 done
 
