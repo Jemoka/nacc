b36e3acaa4100856aa1f23162e0989d57abaf885
Houjun Liu <houjun@jemoka.com>
2024-12-15 23:11:30 -0800

    write full spec

--- experiments/e1/e1_1024.sh
+++ experiments/e1/e1_1024.sh
@@ -8,7 +8,7 @@ HIDDEN=1024 # we will sweep through various hidden dimensions
 
 for lr in 5e-3 5e-4 5e-5 5e-6; do
     for featureset in neuralpsych combined; do
-        CUDA_VISIBLE_DEVICES=2 pytho2 main.py e0_$HIDDEN_$LR --hidden_dim $HIDDEN --n_layers $NLAYERS --batch_size $BATCH_SIZE --lr $lr --featureset $featureset --wandb 
+        CUDA_VISIBLE_DEVICES=2 uv run python main.py e0_$HIDDEN_$LR --hidden_dim $HIDDEN --n_layers $NLAYERS --batch_size $BATCH_SIZE --lr $lr --featureset $featureset --wandb 
     done
 done
 
