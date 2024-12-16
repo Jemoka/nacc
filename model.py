0498a02f2b9ff58db5efe3b13972679e3f5df937
Houjun Liu <houjun@jemoka.com>
2024-12-15 23:07:50 -0800

    get rid of useless posembeds

--- model.py
+++ model.py
@@ -41,27 +41,7 @@ class NACCTemporalLSTM(nn.Module):
 
         # precompute position embeddings
         # we assume that no samples span more than 50 years
-        self._posembds = self.__positionalencoding1d(hidden, 50)
         self._nlayers = nlayers
-
-    @staticmethod
-    def __positionalencoding1d(d_model, length_max):
-        """
-        :param d_model: dimension of the model
-        :param length: length of positions
-        :return: length*d_model position matrix
-        """
-        if d_model % 2 != 0:
-            raise ValueError("Cannot use sin/cos positional encoding with "
-                             "odd dim (got dim={:d})".format(d_model))
-        pe = torch.zeros(length_max, d_model)
-        position = torch.arange(0, length_max).unsqueeze(1)
-        div_term = torch.exp((torch.arange(0, d_model, 2, dtype=torch.float) *
-                             -(math.log(10000.0) / d_model)))
-        pe[:, 0::2] = torch.sin(position.float() * div_term)
-        pe[:, 1::2] = torch.cos(position.float() * div_term)
-
-        return pe
 
     def forward(self, xs, temporal_mask):
         res = torch.zeros((xs.size(0), xs.size(-1)), device=xs.device)
