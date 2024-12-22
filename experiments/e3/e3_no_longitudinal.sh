4805522795a849493c6d51cf8764284e14a5e47b
Houjun Liu <houjun@jemoka.com>
2024-12-21 21:13:33 -0800

    move gpus

--- experiments/e3/e3_no_longitudinal.sh
+++ experiments/e3/e3_no_longitudinal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for lr in 5e-4 5e-5 5e-6; do
-    CUDA_VISIBLE_DEVICES=4 uv run python main.py "e3_no_longitudinal_${lr}" --featureset neuralpsych  --no_longitudinal --lr $lr --validation_interval 64 --checkpoint_interval 64 --report_interval 16 --wandb
+    CUDA_VISIBLE_DEVICES=7 uv run python main.py "e3_no_longitudinal_${lr}" --featureset neuralpsych  --no_longitudinal --lr $lr --validation_interval 64 --checkpoint_interval 64 --report_interval 16 --wandb
 done
 
